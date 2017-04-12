e2b9614cf96e2c0a588da327d49ea632297f85c4
Ryosuke Okada <tobiuokun@gmail.com>
2017-04-12 11:36:11 +0000

    “dive09”

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,13 @@
       n = n + 1
 end
 
+100.times do |n|
+  email = Faker::Internet.email
+  name = Faker::Internet.user_name
+  password = "password"
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
