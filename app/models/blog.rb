035df921396ca9a3f8f1889a4146ccb3595d462c
岡田亮介 <tobiuokun@gmail.com>
2017-04-30 20:11:22 +0900

    コメント機能追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,6 @@
 class Blog < ActiveRecord::Base
     belongs_to :user
+
+    has_many :comments, dependent: :destroy
     validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
+
+  has_many :comments, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
@@ -49,13 +51,5 @@ class User < ActiveRecord::Base
     SecureRandom.uuid
   end
 
-  def update_with_password(params, *options)
-    if provider.blank?
-      super
-    else
-      params.delete :current_password
-      update_without_password(params, *options)
-    end
-  end
 
 end
