11af61bf997254a3f7a105ec3fd625117093955f
岡田亮介 <tobiuokun@gmail.com>
2017-09-05 12:40:10 +0900

    Change applicaion.rb

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,8 @@ module Achieve
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
 
     config.generators do |g|
+      g.assets    false
+      g.helper    false
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
